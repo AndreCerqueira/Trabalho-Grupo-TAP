626d94bb21cfdc1652b95be1f6fd77ac95735763
André Cerqueira <78262628+AndreCerqueira@users.noreply.github.com>
2025-05-16 14:57:49 +0100

    .

--- Assets/ShockEffect/ShockOnCollision.cs
+++ Assets/ShockEffect/ShockOnCollision.cs
@@ -9,8 +9,16 @@ namespace ShockEffect
         public float ShockDuration = 1f;
 
         private float _shockTimer = -1f;
-        private Vector2 _shockCentreUV = new Vector2(0.5f, 0.5f);
+        private Vector2 _shockCenterUV = new Vector2(0.5f, 0.5f);
+        private Mesh _mesh;
+        private MeshCollider _meshCollider;
 
+        void Start()
+        {
+            _meshCollider = GetComponent<MeshCollider>();
+            _mesh = GetComponent<MeshFilter>().mesh;
+        }
+        
         void Update()
         {
             if (_shockTimer >= 0f)
@@ -19,7 +27,7 @@ namespace ShockEffect
                 float t = _shockTimer / ShockDuration;
 
                 ShockMaterial.SetFloat("_TimeParam", t);
-                ShockMaterial.SetVector("_Center", _shockCentreUV);
+                ShockMaterial.SetVector("_Center", _shockCenterUV);
 
                 if (t >= 1f)
                     _shockTimer = -1f;
@@ -29,14 +37,32 @@ namespace ShockEffect
 
         private void OnCollisionEnter(Collision collision)
         {
-            TriggerShock();
+            Vector3 point = collision.contacts[0].point;
+            Vector2 uv = WorldToMeshUV(point);
+            Debug.Log($"Converted UV: {uv}");
+            TriggerShock(uv);
         }
         
         
-        public void TriggerShock()
+        public void TriggerShock(Vector2 uv)
         {
-            _shockCentreUV = new Vector2(0.5f, 0.5f);
+            _shockCenterUV = uv;
             _shockTimer = 0f;
+        }
+
+        private Vector2 WorldToMeshUV(Vector3 worldPoint)
+        {
+            Ray ray = new Ray(worldPoint + Vector3.up * 0.01f, Vector3.down);
+            RaycastHit hit;
+
+            if (_meshCollider.Raycast(ray, out hit, 1f))
+            {
+                return hit.textureCoord;
+            }
+
+            // fallback
+            Debug.Log("WorldToMeshUV falhou. A usar centro padrão.");
+            return new Vector2(0.5f, 0.5f);
         }
     }
 }
